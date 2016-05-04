755b938d431301dc18591b03d9c11d74cdb64af6
sirsharpest <nathan1hughes@gmail.com>
2016-05-04 18:58:30 +0100

    Refactoring in preperation for latest version

--- main.py
+++ main.py
@@ -10,22 +10,142 @@
 import os
 import urllib.request
 import http.cookiejar
-import blackboard_loader
 from os.path import expanduser
 import time
 from threading import Thread
 import getpass
+import urllib.request
+import urllib.parse
+import bs4
+from mimetypes import guess_extension
+from documents import pdfFile, bbFolder
+import re
+
+
 
 ################################################
-# global vars (plz don't shout at me)
-################################################
-user_id_box = 'user_id'
-user_passwd_box = 'password'
+# Functions
+###############################################
 
-user = input('Enter in Aber ID: ')
-passwd = getpass.getpass('Enter password: ')
-home = expanduser('~/Documents')
-login_bttn = 'login'
+def login_bb(user_id, user_passwd):
+    authentication_url = 'https://blackboard.aber.ac.uk/webapps/login/'
+    payload = {
+        'op': 'login',
+        'user_id': user_id,
+        'password': user_passwd
+    }
+    print('Entering Blackboard...')
+    data = urllib.parse.urlencode(payload)
+    binary_data = data.encode('UTF-8')
+    req = urllib.request.Request(authentication_url, binary_data)
+    resp = urllib.request.urlopen(req)
+    contents = resp.read()
+    print('Connected!')
+
+
+# Does just that, finds a file pdf,ppt or pptx and saves it
+def download_file(file_to_get):
+    print('Accessing: ' + file_to_get.get_name())
+    source = urllib.request.urlopen(file_to_get.get_url())
+    app_name = file_to_get.get_name().encode('utf-8')
+    file = open(app_name, 'wb')
+    file.write(source.read())
+    file.close()
+
+    print('File saved: ' + file_to_get.get_name())
+
+
+# This will print the links which have "pdf" specified in the naming
+def get_links(url):
+    site = urllib.request.urlopen(url)
+    html = site.read()
+    # parse the html
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+
+    data = soup.find_all(id='content')
+
+    # container for the docs
+    documents = []
+
+    for div in data:
+        links = div.find_all('a')
+        for a in links:
+            if '.pdf' in a.text.lower() or '.ppt' in a.text.lower() or '.pptx' in a.text.lower() or '.zip' in a.text.lower() or '.class' in a.text.lower():
+
+                temp_doc = pdfFile()
+                temp_doc.set_name(a.text)
+                temp_doc.set_url('https://blackboard.aber.ac.uk' + a['href'])
+                if 'dcswww' in temp_doc.get_url():  # ignore if its on the aber server as cannot access
+                    continue
+                if 'http://www.cokeandcode.com/main/tutorials/path-finding/' in temp_doc.get_name():  # messy fix needs corrected soon
+                    continue
+                documents.append(temp_doc)
+
+    return documents
+
+
+def get_folder_links(url, div):
+    site = urllib.request.urlopen(url)
+    html = site.read()
+
+    # parse the html
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+
+    data = soup.find_all(id=div)
+
+    # container for the docs
+    documents = []
+
+    # messy temp variable to avoid confusion with for loop
+    temp = div
+
+    for div in data:
+        links = div.find_all('a')
+        for a in links:
+            if 'listContent' in a['href'] or 'execute' in a['href']:
+                folder = bbFolder()
+                folder.set_name(a.text)
+                if '/' in folder.get_name():
+                    folder.set_name(folder.get_name().replace('/', ' '))
+                    # folder.set_name(str(folder.get_name()).replace('/', '\\'))  # fixes bug of its making extra folder
+                folder.set_url('https://blackboard.aber.ac.uk' + a['href'])
+
+                if temp == 'module:_371_1':
+                    module_pattern = re.compile('[a-zA-Z]{2}\d{5}')
+                    if module_pattern.search(a.text) is not None:
+                        print('Found a module ' + a.text)
+                        documents.append(folder)
+                    continue
+
+
+                documents.append(folder)
+
+    return documents
+
+
+#TODO REWRITE THIS TO SEARCH ALL FOLDERS
+#This function should get all of the links in the sidebar
+def find_content_link(url):
+    site = urllib.request.urlopen(url)
+    html = site.read()
+
+    # parse the html
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+
+    # data = soup.find_all(id='content')
+    data = soup.find_all(id='menuWrap')
+
+    for div in data:
+        links = div.find_all('a')
+        for a in links:
+
+            #if its not a realitive link then I want to avoid it
+            if 'www.' in a['href']:
+                continue
+
+            return ('https://blackboard.aber.ac.uk' + a['href'])
+
+
 
 ################################################
 # Main
@@ -33,6 +153,16 @@ login_bttn = 'login'
 
 
 #Just doing some setup stuff
+
+user_id_box = 'user_id'
+user_passwd_box = 'password'
+
+user = input('Enter in Aber ID: ')
+passwd = input('Enter password: ')
+home = expanduser('~/Documents')
+login_bttn = 'login'
+
+
 cj = http.cookiejar.CookieJar()
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
@@ -41,74 +171,10 @@ start_time = time.time()
 os.chdir(home)
 
 
-################################################
-# Functions
-###############################################
-
-
-def get_all(url):
-    files = blackboard_loader.get_links(url)
-    folders = blackboard_loader.get_folder_links(url, 'content')
-
-    for i in folders:
-        print(i.get_name())
-
-    if folders:
-        for folder in folders:
-            if not os.path.exists(folder.get_name()):
-                print('Making sub-folder for:'+folder.get_name())
-                os.makedirs(folder.get_name())
-            os.chdir(folder.get_name())
-            get_all(folder.get_url())
-            os.chdir('..')
-
-    Threads = []
-    for item in files:
-
-        t = Thread(blackboard_loader.download_file(item))
-        Threads.append(t)
-
-        #print(item.get_url() + ' found at: ' + url )
-        #print('Downloading now')
-        #blackboard_loader.download_file(item)
-
-    for t in Threads:
-        t.start()
-
-    for t in Threads:
-        t.join()
-
-
-def explore_pages(pages):
-    for links in pages:
-        if not links.get_url(): #if the link hasn't been picked up then gloss over it (hopefully)
-            continue
-        if not os.path.exists(links.get_name()):
-            print('Making directory for: ' + links.get_name())
-            os.makedirs(links.get_name())
-        os.chdir(links.get_name())
-        get_all(links.get_url())
-        if not os.listdir('../'+links.get_name()):
-            os.chdir('../')
-            os.rmdir(links.get_name())
-        os.chdir(home)
-
-
-
 # Call to login to blackboard
-blackboard_loader.login_bb(user, passwd)
-
+login_bb(user, passwd)
 
 modules_container = 'https://blackboard.aber.ac.uk/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_55_1'
-modules_folders = blackboard_loader.get_folder_links(modules_container, 'module:_371_1')
 
-for link in modules_folders:
-    link.set_url(blackboard_loader.find_content_link(link.get_url()))
-
-explore_pages(modules_folders)
-
-
-end_time = time.time()
-total_time = end_time - start_time
-print('Finished in: ' + str(total_time) + ' seconds')
+modules_folders = get_folder_links(modules_container, 'module:_371_1')
 
